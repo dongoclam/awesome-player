5c31787ffb13594cf8138040b552dc34f2aefcc9
Lam Do <dongoclam1993@gmail.com>
2019-07-09 00:04:33 +0700

    Version 2

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const favicon = require('serve-favicon')
--- services/uploader.js
+++ services/uploader.js
@@ -1,3 +1,5 @@
+const path = require('path')
+const fs = require('fs')
 const formidable = require('formidable')
 const config = require('../config')
 
@@ -12,9 +14,12 @@ Uploader.form.uploadDir = config.uploadDir
 Uploader.form.maxFileSize = 200 * 1024 * 1024;
 
 Uploader.saveFile = function(req, res) {
+
+  fs.existsSync(config.uploadDir) || fs.mkdirSync(config.uploadDir)
+
   this.form.onPart = (part) => {
     if (!part.filename || part.mime.match(/audio\/|video\//)) {
-      this.form.handlePart(part);
+      this.form.handlePart(part)
     } else {
       res.send({error: 'File type not allowed'})
     }
@@ -32,7 +37,7 @@ Uploader.saveFile = function(req, res) {
 
   return new Promise((resolve, reject) => {
     this.form.on('file', (name, file) => {
-      resolve(file)
+      resolve(path.basename(file.path))
     })
   })
 }
